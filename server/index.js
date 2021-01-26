06ef98c45f863a585948b880940630442f89a6df
Kostis <polu_kostas@yahoo.gr>
2021-01-26 20:23:30 +0200

    file structure change

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-require('../services/passport');
-const authRoutes= require('../routes/authRoutes');
+require('./services/passport');
+const authRoutes= require('./routes/authRoutes');
 
 authRoutes(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../server/keys');
+const keys = require('../keys');
 
 passport.use( new GoogleStrategy({
     clientID: keys.googleClientID,
