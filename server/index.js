523c96df8c5548dc24383c8dda02dd1d99df4f64
Kostis <polu_kostas@yahoo.gr>
2021-01-27 14:24:13 +0200

    mongo uri and giignore root file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose=require('mongoose');
 const app = express();
 require('./services/passport');
 const authRoutes= require('./routes/authRoutes');
 
+mongoose.connect('mongodb+srv://Kostis:atlas987654321coolsend!@cluster0.orx5r.mongodb.net/test?retryWrites=true&w=majority');
+
 authRoutes(app);
+
+app.get('/', () => {
+    message: 'root route'
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port);
