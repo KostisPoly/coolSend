f2af6c5e8d65bb9b0587013f7eaf32cedc29dc63
Kostis <polu_kostas@yahoo.gr>
2021-02-09 20:25:54 +0200

    current user action creator and proxy and route added

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware("/auth/google", { target: "http://localhost:5000/" })
+        createProxyMiddleware(["/auth/google", "/api/*"], { target: "http://localhost:5000/" })
     );
 };
